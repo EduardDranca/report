201ff8fa0d8fe64dab60b0991f7c7ada9b8df72d
ed <eddranca@gmail.com>
2024-01-09 21:04:56 +0200

    CI.yml

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,10 +1,19 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class MainTest {
     @Test
     public void test() {
 
+    }
+
+    @Test
+    public void test2() {
+        String var1 = System.getenv("var1");
+        String var2 = System.getenv("var2");
+        Assertions.assertEquals("abc", var1);
+        Assertions.assertEquals("def", var2);
     }
 }
